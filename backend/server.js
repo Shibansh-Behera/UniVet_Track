627c6ac31b437087d6806dad9189ee8b72fb9304
Amrit-Raj-Biswal <166406479+Amrit-Raj-Biswal@users.noreply.github.com>
2025-11-04 16:57:03 +0530

    Edited Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,22 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import connectDB from "./config/db.js";
-import userRoutes from "./routes/UserRoutes.js";
-import reportRoutes from "./routes/ReportRoutes.js";
-import adminRoutes from "./routes/AdminRoutes.js";
-import adminReportRoutes from "./routes/AdminRoutes.js";
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./config/db.js"
+import userRouter from "./routes/UserRoutes.js"
+import reportRouter from "./routes/ReportRoutes.js"
+import adminRouter from "./routes/AdminRoutes.js"
 
-dotenv.config();
-connectDB();
+dotenv.config()
+connectDB()
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req, res) => res.send("🐾 Animal Rescue API Running..."));
-app.use("/api/admin/reports", adminReportRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/reports", reportRoutes);
+app.get("/", (req, res) => res.send("🐾 Animal Rescue API Running..."))
+app.use("/api/admin", adminRouter)
+app.use("/api/users", userRouter)
+app.use("/api/reports", reportRouter)
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
