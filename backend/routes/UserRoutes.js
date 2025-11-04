3f0fafb952860b8730d42be57f99449f07c432b6
Amrit-Raj-Biswal <166406479+Amrit-Raj-Biswal@users.noreply.github.com>
2025-11-04 15:47:22 +0530

    minor edits 4

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,9 +1,9 @@
-import express from "express";
-import { registerUser, loginUser } from "../controllers/UserController.js";
+import express from "express"
+import { registerUser, loginUser } from "../controllers/UserController.js"
 
-const router = express.Router();
+const router = express.Router()
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/register", registerUser)
+router.post("/login", loginUser)
 
-export default router;
+export default router
